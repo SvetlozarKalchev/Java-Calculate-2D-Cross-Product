50361b700c390cc6883e9cf3de7341797ffc6f22
SvetlozarKalchev <svetlozar@kalchev.eu>
2016-03-03 15:06:20 +0100

    Add method that creates matrices

---
+++ MatrixCreator.java
@@ -0,0 +1,11 @@
+public class MatrixCreator
+{
+  // Creates a matrix with the desired x,y size
+  public void createMatrix()
+  {
+
+  }
+
+  // Generates random floating point value 0 < n < range
+  public double generateValue(int range)
+}
