64f4319f5781b6ad8e41f605a45b8ee2566b90b5
Georgi Mavrov <mavrovgeorgi@yahoo.com>
2016-03-03 14:51:56 +0100

    Edit

--- Hello.java
+++ Hello.java
@@ -2,6 +2,13 @@ public class Hello
 {
   public static void main(String args[])
   {
-    System.out.print("Hello World!");
+    System.out.println("Hello World!");
+    System.out.println("Guten Tag!");
+
+    int a = 5;
+    String first = "georgi";
+
+    System.out.println(a);
+    System.out.println(first);
   }
 }
